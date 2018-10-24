aecbc79bf75bb489d758dc372897829dcd33404d
Trit <zamax27@gmail.com>
2018-10-25 00:09:25 +0200

    On continue les tentatives de débug

--- EOS MK2/kernel/kernel.c
+++ EOS MK2/kernel/kernel.c
@@ -2,9 +2,10 @@
 
 void main() { //ah montre le fichier
   clear_screen();
-  kprint_at("X", 1, 6);
-  kprint_at("Ce texte est sur plusieurs lignes", 75, 10);
-  kprint_at("Il y a un retour a\n la ligne", 0, 20);
-  kprint("Ce texte à un re\ntour a la ligne");
-  kprint_at("Que se passe-t-il quand on n'a plus de place ?", 45, 24);
+  print_char('d', 5, 5, 0x0f);
+  kprint_at("X", 1, 9);
+  //kprint_at("Ce texte est sur plusieurs lignes", 75, 10);
+  //kprint_at("Il y a un retour a\n la ligne", 0, 20);
+  //kprint("Ce texte à un re\ntour a la ligne");
+  //kprint_at("Que se passe-t-il quand on n'a plus de place ?", 45, 24);
 }
