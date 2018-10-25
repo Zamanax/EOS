ca33dfc752f2bbfcdd27c4045c6a0d4768fe96bf
dogasp <37941592+dogasp@users.noreply.github.com>
2018-10-25 16:59:29 +0200

    inconsistances

--- EOS MK2/kernel/kernel.c
+++ EOS MK2/kernel/kernel.c
@@ -1,10 +1,13 @@
-#include "/home/zamani/Documents/WorkspaceRepo/ISN/OS/EOS MK2/drivers/screen.h"
+#include "../drivers/screen.h"
 
-void main() { //ah montre le fichier
+void main() { //fonction principale de l'os
   clear_screen();
-  fprint("x", 20, 20);
-  print_char('d', 2, 5, 0x0f);
-  kprint_at("X", 85, 85);
+  char test[2] = {'e', '\0'};  //pour le test, on créé une vairable "à la main"
+  fprint(test, 2, 1);         //fonctionne
+  kprint_at(test, 2, 3);      //rien
+  
+  print_char('d', 2, 5, 0x0a); //fonctionne
+  //kprint_at("d", 3, 1);
   //kprint_at("Ce texte est sur plusieurs lignes", 75, 10);
   //kprint_at("Il y a un retour a\n la ligne", 0, 20);
   //kprint("Ce texte à un re\ntour a la ligne");
